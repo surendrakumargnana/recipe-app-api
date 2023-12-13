d6a7d28388baf0b3f615618eeb2e044caf911921
surendrakumargnana <surendrakumargnanapragasam@gmail.com>
2023-12-13 10:52:08 +0530

    Admin setup linting fix

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,7 +3,7 @@ Django admin customization.
 """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.utils.translation import gettext_lazy as _ # translation support
+from django.utils.translation import gettext_lazy as _  # translation support
 
 from core import models
 
@@ -24,7 +24,7 @@ class UserAdmin(BaseUserAdmin):
                 )
             }
         ),
-        (_('Important dates'), {'fields':('last_login',)}),
+        (_('Important dates'), {'fields': ('last_login',)}),
     )
     readonly_fields = ['last_login']
     add_fieldsets = (
