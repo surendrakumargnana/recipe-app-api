d66d4c32359fa9ae8c28d070647a570ba189b241
surendrakumargnana <surendrakumargnanapragasam@gmail.com>
2023-12-29 00:15:23 +0530

    tag api endpoint added

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -10,6 +10,11 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
+def create_user(email='user@example.com', password='password@123'):
+    """Create and return a new user."""
+    return get_user_model().objects.create_user(email=email, password=password)
+
+
 class ModelTests(TestCase):
     """Test models."""
 
@@ -69,3 +74,10 @@ class ModelTests(TestCase):
         )
 
         self.assertEquals(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
